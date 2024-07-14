d2a4c9cebd157693f20322c0ece1b0158816143e
謝明峰 <show32112tw@gmail.com>
2024-07-14 06:52:12 +0000

    modify

--- linebot_test/main.py
+++ linebot_test/main.py
@@ -1,3 +1,4 @@
+from cgitb import handler
 from flask import Flask,request, abort
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookHandler
@@ -7,15 +8,11 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>我的第一個網站-修改目前網站</h1>"
-
-@app.route("/pwd")
-def pwd():
-    return f'''
-    <h2>{os.environ['CHANNEL_ACCESS_TOKEN']}</h2>
-    <h2>{os.environ['CHANNEL_SECRET']}</h2>
-    <h2>{os.environ['Gemini_API_KEY']}</h2>
-    '''
